ae67bb13ad33782c4334cb1115482b446cede34e
Lachee <lachlan.h@hotmail.com>
2019-10-24 11:00:03 +1100

    Rough Draft

---
+++ UnityPackage/Extension/TarOutputStreamExtensions.cs
@@ -0,0 +1,125 @@
+﻿using ICSharpCode.SharpZipLib.Tar;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace UnityPackage.Extension
+{
+    public static class TarOutputStreamExtensions
+    {
+        /// <summary>
+        /// Writes a file to the stream
+        /// </summary>
+        /// <param name="stream"></param>
+        /// <param name="source"></param>
+        /// <param name="dest"></param>
+        public static void WriteFile(this TarOutputStream stream, string source, string dest)
+        {
+            using (Stream inputStream = File.OpenRead(source))
+            {
+                long fileSize = inputStream.Length;
+
+                // Create a tar entry named as appropriate. You can set the name to anything,
+                // but avoid names starting with drive or UNC.
+                TarEntry entry = TarEntry.CreateTarEntry(dest);
+
+                // Must set size, otherwise TarOutputStream will fail when output exceeds.
+                entry.Size = fileSize;
+
+                // Add the entry to the tar stream, before writing the data.
+                stream.PutNextEntry(entry);
+
+                // this is copied from TarArchive.WriteEntryCore
+                byte[] localBuffer = new byte[32 * 1024];
+                while (true)
+                {
+                    int numRead = inputStream.Read(localBuffer, 0, localBuffer.Length);
+                    if (numRead <= 0)
+                        break;
+
+                    stream.Write(localBuffer, 0, numRead);
+                }
+
+                //Close the entry
+                stream.CloseEntry();
+            }
+        }
+
+        /// <summary>
+        /// Writes a text file to the stream
+        /// </summary>
+        /// <param name="stream"></param>
+        /// <param name="dest"></param>
+        /// <param name="content"></param>
+        public static void WriteAllText(this TarOutputStream stream, string dest, string content)
+        {
+            byte[] bytes = System.Text.Encoding.UTF8.GetBytes(content);
+            
+            TarEntry entry = TarEntry.CreateTarEntry(dest);
+            entry.Size = bytes.Length;
+
+            // Add the entry to the tar stream, before writing the data.
+            stream.PutNextEntry(entry);
+
+            // this is copied from TarArchive.WriteEntryCore
+            stream.Write(bytes, 0, bytes.Length);
+
+            //Close the entry
+            stream.CloseEntry();
+        }
+
+        /// <summary>
+        /// Reads a file from a stream
+        /// </summary>
+        /// <param name="tarIn"></param>
+        /// <param name="outStream"></param>
+        /// <returns></returns>
+        public static long ReadNextFile(this TarInputStream tarIn, Stream outStream)
+        {
+            long totalRead = 0;
+            byte[] buffer = new byte[4096];
+            bool isAscii = true;
+            bool cr = false;
+
+            int numRead = tarIn.Read(buffer, 0, buffer.Length);
+            int maxCheck = Math.Min(200, numRead);
+
+            totalRead += numRead;
+
+            for (int i = 0; i < maxCheck; i++)
+            {
+                byte b = buffer[i];
+                if (b < 8 || (b > 13 && b < 32) || b == 255)
+                {
+                    isAscii = false;
+                    break;
+                }
+            }
+
+            while (numRead > 0)
+            {
+                if (isAscii)
+                {
+                    // Convert LF without CR to CRLF. Handle CRLF split over buffers.
+                    for (int i = 0; i < numRead; i++)
+                    {
+                        byte b = buffer[i];     // assuming plain Ascii and not UTF-16
+                        if (b == 10 && !cr)     // LF without CR
+                            outStream.WriteByte(13);
+                        cr = (b == 13);
+
+                        outStream.WriteByte(b);
+                    }
+                }
+                else
+                    outStream.Write(buffer, 0, numRead);
+
+                numRead = tarIn.Read(buffer, 0, buffer.Length);
+                totalRead += numRead;
+            }
+
+            return totalRead;
+        }
+    }
+}
---
+++ UnityPackage/Package.cs
@@ -0,0 +1,74 @@
+﻿using ICSharpCode.SharpZipLib.GZip;
+using ICSharpCode.SharpZipLib.Tar;
+using System;
+using System.IO;
+using System.Text;
+using UnityPackage.Extension;
+
+namespace UnityPackage
+{
+    public class Package
+    {
+        public byte[] Pack()
+        {
+            using (var memoryStream = new MemoryStream())
+            {
+                using (var gzoStream = new GZipOutputStream(memoryStream))
+                using (var tarStream = new TarOutputStream(gzoStream))
+                {
+
+                }
+                    
+                return memoryStream.ToArray();
+            }
+        }
+
+        /// <summary>
+        /// Packs a single asset into the stream.
+        /// </summary>
+        /// <param name="stream">The stream to pack the asset into</param>
+        /// <param name="root">The root Unity project.</param>
+        /// <param name="asset">The asset file</param>
+        /// <returns></returns>
+        private bool PackAsset(TarOutputStream stream, string root, string asset)
+        {
+            //Skip meta files
+            if (Path.GetExtension(asset).ToLowerInvariant() == ".meta")
+                return false;
+
+            //Skip missing file
+            if (!File.Exists(asset))
+                return false;
+
+            string meta = $"{asset}.meta";
+            string metaContent = null;
+            string guidString = null;
+
+            //Get the GUID
+            if (!File.Exists(meta))
+            {
+                StringBuilder guidBuilder = new StringBuilder();
+
+                //Manually create a GUI string
+                Guid guid = Guid.NewGuid();
+                foreach (var byt in guid.ToByteArray())
+                    guidBuilder.Append(string.Format("{0:X2}", byt));
+
+                guidString = guidBuilder.ToString();
+                metaContent = $"guid: {guidString}\n";
+            }
+            else
+            {
+                metaContent = File.ReadAllText(meta);
+                guidString = metaContent.Substring(metaContent.IndexOf("guid: ") + 6, 32);
+            }
+
+            //Write to the stream
+            stream.WriteFile(asset, $"{guidString}/asset");
+            stream.WriteAllText($"{guidString}/asset.meta", metaContents);
+            stream.WriteAllText($"{guidString}/pathname", relativePath.Replace('\\', '/'));
+
+            return true;
+        }
+    }
+}
