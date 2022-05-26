64fb57e60e076de8e0677f4e81ac8b234075f318
Lachee <lachlan.h@hotmail.com>
2022-05-26 12:45:02 +1000

    Added async

--- UnityPackageExporter/DependencyAnalyiser.cs
+++ UnityPackageExporter/DependencyAnalyiser.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace UnityPackageExporter
 {
@@ -29,7 +30,7 @@ namespace UnityPackageExporter
             project = workspace.AddProject(projectInfo);
         }
 
-
+        /// <summary>Adds a file</summary>
         public void AddFile(string file)
         {
             var name = Path.GetFileName(file);
@@ -39,12 +40,35 @@ namespace UnityPackageExporter
             project = doc.Project;
         }
 
-        public void FindDependencies(string file)
+        /// <summary>Adds a file</summary>
+        public async Task AddFileAsync(string file)
+        {
+            var name = Path.GetFileName(file);
+            var fileContent = await File.ReadAllTextAsync(file);
+            var src = SourceText.From(fileContent);
+            var doc = project.AddDocument(name, src, filePath: file);
+            project = doc.Project;
+        }
+
+        public async Task FindDependenciesAsync(string file)
         {
             // Clear WOrkspace
             workspace.TryApplyChanges(solution);
+            await Task.CompletedTask;
 
             /*
+             * ASYNC
+            var model = await sourceDocument.GetSemanticModelAsync();                                                           //Get the semantic model
+            var root = await sourceDocument.GetSyntaxRootAsync();                                                               //Get the syntax
+            var syntax = root.DescendantNodes().OfType<ClassDeclarationSyntax>().First();                                       //Find the first ClassDeclaration within the syntax
+            var symbol = model.GetDeclaredSymbol(syntax);                                                                       //Get the symbol based of the class declaration
+
+            var references = await SymbolFinder.FindReferencesAsync(symbol, sourceDocument.Project.Solution);                               //Find references
+            return references.SelectMany(s => s.Locations).Select(loc => new AnalyticResult(loc));
+            */
+
+            /*
+             * SYNC
                 //Find the model
                 var modelAwait = sourceDocument.GetSemanticModelAsync().ConfigureAwait(false);
                 while (!modelAwait.GetAwaiter().IsCompleted) yield return State.FindingModel;
